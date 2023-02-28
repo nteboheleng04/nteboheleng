0aff5cb6da6299af818418690eeb3dd3ba2a3c0a
nteboheleng04 <95362589+nteboheleng04@users.noreply.github.com>
2023-02-28 15:59:18 +0200

    Add files via upload

---
+++ MALEETO KUENA,901013387/MALEETO KUENA/MALEETO KUENA/Module1.vb
@@ -0,0 +1,31 @@
+﻿Option Strict On
+Option Explicit On
+Module Module1
+
+    Sub Main()
+        'calling a method
+        Call enterAndStore(elements)
+
+    End Sub
+    Sub enterAndStore(ByVal elements() As Double)
+        'declare variable
+        Dim elements() As Double={100.6,24.7,23.8};
+                                 {56.3,158.4,111.0}
+        Dim sum As Double
+
+
+                Console.WriteLine(sum))
+
+
+            Next
+
+        Next
+
+    End Sub
+
+
+
+
+
+
+End Module
